080c41ce31817b518717cced32561043ddeb06a6
Daniel Zenteno <dzenteno@conabio.gob.mx>
2015-03-08 11:21:22 -0600

    Testing

--- app/src/main/java/mx/labcd/hack/taxiandroid/Camera.java
+++ app/src/main/java/mx/labcd/hack/taxiandroid/Camera.java
@@ -1,7 +1,10 @@
 package mx.labcd.hack.taxiandroid;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.location.Location;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
@@ -9,20 +12,80 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.File;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 
-public class Camera extends ActionBarActivity {
+public class Camera extends ActionBarActivity
+        implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener, OnMapReadyCallback{
 
     private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
     private static final int CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE = 200;
     private Uri fileUri;
     public static final int MEDIA_TYPE_IMAGE = 1;
     public static final int MEDIA_TYPE_VIDEO = 2;
+
+    private GoogleApiClient mGoogleApiClient;
+    private Location mCurrentLocation;
+    private String mLastUpdateTime;
+    private LocationRequest mLocationRequest;
+
+    private MapFragment mapFragment;
+
+    private static final int userId = 1;
+    private static final String taxiMapUrl = "http://taximap-ezentenoj.rhcloud.com/";
+
+    private boolean rideStarted;
+    private int rideId;
+
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    protected synchronized void buildGoogleApiClient(){
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        mGoogleApiClient.connect();
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,10 +95,10 @@ public class Camera extends ActionBarActivity {
         //Create Intent
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
-        fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
-
         startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+
+
+
 
     }
 
@@ -105,9 +168,19 @@ public class Camera extends ActionBarActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
             if (resultCode == RESULT_OK) {
+
+                buildGoogleApiClient();
+                createLocationRequest();
+
                 // Image captured and saved to fileUri specified in the Intent
-                Toast.makeText(this, "Image saved to:\n" +
-                        data.getData(), Toast.LENGTH_LONG).show();
+                Bundle extras = data.getExtras();
+                Bitmap imageBitmap = (Bitmap) extras.get("data");
+                ImageView imgView = (ImageView) findViewById(R.id.plateView);
+                imgView.setImageBitmap(imageBitmap);
+
+                mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+                mapFragment.getMapAsync(this);
+
             } else if (resultCode == RESULT_CANCELED) {
                 // User cancelled the image capture
             } else {
@@ -127,5 +200,158 @@ public class Camera extends ActionBarActivity {
             }
         }
     }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        // Connected to Google Play services!
+        // The good stuff goes here.
+        startLocationUpdates();
+    }
+
+    protected void startLocationUpdates(){
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocationRequest, this
+        );
+    }
+
+    protected void stopLocationUpdates() {
+        LocationServices.FusedLocationApi.removeLocationUpdates(
+                mGoogleApiClient, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        // The connection has been interrupted.
+        // Disable any UI components that depend on Google APIs
+        // until onConnected() is called.
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        // This callback is important for handling errors that
+        // may occur while attempting to connect with Google.
+        //
+        // More about this in the next section.
+        Log.d("tag","NO SE PUDO CONECTAR");
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mCurrentLocation = location;
+        DateFormat.getTimeInstance().format(new Date());
+
+        new MyAsyncTask().execute("");
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng latLng = new LatLng(19.375448, -99.256035);
+        googleMap.addMarker(new MarkerOptions()
+        .position(latLng)
+        .title("EseTaxi"));
+
+    }
+
+
+    private class MyAsyncTask extends AsyncTask<String, Integer, Double> {
+
+        @Override
+        protected Double doInBackground(String... params) {
+            // TODO Auto-generated method stub
+            postData();
+            return null;
+        }
+
+        protected void onPostExecute(Double result){
+            Log.d("tag", "Dont know");
+        }
+        protected void onProgressUpdate(Integer... progress){
+
+        }
+
+        public void postData() {
+            if(!rideStarted)
+                startRide();
+            else
+                continueRide();
+
+        }
+
+        private void startRide(){
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpResponse response = null;
+
+            String setCoordinate = "setCoordinate.php?";
+            String postUrl = taxiMapUrl + setCoordinate;
+            postUrl += "user_id=" + String.valueOf(userId);
+            postUrl += "&coordinate=" + String.valueOf(mCurrentLocation.getLatitude()) + ",";
+            postUrl += "%20" + String.valueOf(mCurrentLocation.getLongitude());
+
+            HttpPost httpPost = new HttpPost(postUrl);
+
+            try{
+                response = httpClient.execute(httpPost);
+                Log.d("TAG", "terminó");
+            }
+            catch (ClientProtocolException e){
+                e.printStackTrace();
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
+
+            String responseText = null;
+            try{
+                responseText = EntityUtils.toString(response.getEntity());
+                Log.d("TAG", responseText);
+                try{
+                    JSONObject json = new JSONObject(responseText);
+                    JSONObject jsonResult = json.getJSONObject("result");
+                    rideId = jsonResult.getInt("ride_id");
+                    rideStarted = true;
+                }
+                catch(JSONException e){
+                    e.printStackTrace();
+                }
+            }
+            catch(IOException e){
+                e.printStackTrace();
+            }
+
+        }
+
+        private void continueRide(){
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpResponse response = null;
+
+            String setCoordinate = "setCoordinate.php?";
+            String postUrl = taxiMapUrl + setCoordinate;
+            postUrl += "user_id=" + String.valueOf(userId);
+            postUrl += "&ride_id=" + String.valueOf(rideId);
+            postUrl += "&coordinate=" + String.valueOf(mCurrentLocation.getLatitude()) + ",";
+            postUrl += "%20" + String.valueOf(mCurrentLocation.getLongitude());
+
+            HttpPost httpPost = new HttpPost(postUrl);
+
+            try{
+                response = httpClient.execute(httpPost);
+                Log.d("TAG", "terminó");
+            }
+            catch (ClientProtocolException e){
+                e.printStackTrace();
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
+
+
+        }
+
+
+
+    }
+
 
 }
